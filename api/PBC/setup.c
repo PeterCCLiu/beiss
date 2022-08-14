d990904b2f61afb6be4bb773e96394e7a1714cf3
peterccliu <peter.cc.liu@cn.ey.com>
2022-08-15 01:33:54 +0800

    "reconstruct pairing from pairing_param_buffer, sync pairing among KGC and IoT devices"

--- api/PBC/setup.c
+++ api/PBC/setup.c
@@ -9,12 +9,12 @@
 
 // Generate params and master pub/priv keys
 
-void setup(){
+void setup(pairing_t p, pbc_param_t par){
 
     // pairing param init
     pbc_param_init_a_gen(par, RBITS ,QBITS); // Type A curve produces symmetric pairing, a necessity for Al-Riyami-Patterson03.
     pairing_init_pbc_param(p, par);
-    if (!pairing_is_symmetric(p)) pbc_die("pairing must be symmetric");
+    if (!pairing_is_symmetric(p)) pbc_die("Al-Riyami-Patterson03 scheme requires a symmetric pairing, exiting.");
 
     /*
      * Type F curve (Barreto and Naehrig 256) has shorter signature/key length, good for IoT. Not yet applicable for Al-Riyami-Patterson03, leave for future work.
@@ -26,7 +26,7 @@ void setup(){
     fp = fopen("pairing_params.txt", "wr+");
     pbc_param_out_str(fp, par);
     fclose(fp);
-    // pairing param init complete, check in params.txt
+    // pairing param init complete, check in pairing_params.txt
 
     // generator init
     element_init_G1(g, p);
@@ -45,7 +45,40 @@ void setup(){
     // master pub/priv keygen complete
 }
 
-void init_test(){
+void setup_with_param_buffer(pairing_t p, char * par_param_buffer, int len){
+    pairing_init_set_buf(p, par_param_buffer, len);
+
+    // generator init
+    element_init_G1(g, p);
+    element_random(g);
+    // generator init complete
+
+    // master pub/priv keygen
+    element_init_Zr(masterPrivateKey, p);
+    element_init_G1(masterPublicKey, p);
+
+    element_random(masterPrivateKey); // How to securely store this after deployment?
+    element_pow_zn(masterPublicKey, g, masterPrivateKey);
+
+    element_init_G1(P0, p);
+    element_set(P0, masterPublicKey);
+    // master pub/priv keygen complete
+}
+
+char * extract_pairing_param_buffer(){
+    char * par_param_buffer = malloc(PARAM_BUFFER_LENGTH_TYPE_A);
+    FILE *fp;
+    fp = fopen("pairing_params.txt", "r");
+    fread(par_param_buffer, PARAM_BUFFER_LENGTH_TYPE_A, 1, fp);
+    fclose(fp);
+
+    puts("Your par_param_buffer is: ");
+    puts(par_param_buffer);
+
+    return par_param_buffer;
+}
+
+void init_test(pairing_t p){
     // printf("Your length in bits to represent a G1 element is = %d\n", pairing_length_in_bytes_G1(p));
     printf("Is pairing is symmetric, 1 is symmetric? : %d\n", pairing_is_symmetric(p));
 
@@ -56,8 +89,18 @@ void init_test(){
     element_printf("Your master sk is = %B\n", masterPrivateKey);
     element_printf("Your master pk is = %B\n", masterPublicKey);
 
-    printf("Your master pk element_length_in_bytes is = %d\n", element_length_in_bytes(masterPrivateKey));
+    printf("Your master sk element_length_in_bytes is = %d\n", element_length_in_bytes(masterPrivateKey));
     printf("Your master pk element_length_in_bytes is = %d\n", element_length_in_bytes(masterPublicKey));
+
+    printf("Your pairing_length_in_bytes_G1(p) is = %d\n", pairing_length_in_bytes_G1(p));
+    printf("Your pairing_length_in_bytes_G2(p) is = %d\n", pairing_length_in_bytes_G2(p));
+    printf("Your pairing_length_in_bytes_compressed_G1(p) is = %d\n", pairing_length_in_bytes_compressed_G1(p));
+    printf("Your pairing_length_in_bytes_compressed_G2(p) is = %d\n", pairing_length_in_bytes_compressed_G2(p));
+    printf("Your pairing_length_in_bytes_x_only_G1(p) is = %d\n", pairing_length_in_bytes_x_only_G1(p));
+    printf("Your pairing_length_in_bytes_x_only_G2(p) is = %d\n", pairing_length_in_bytes_x_only_G2(p));
+    printf("Your pairing_length_in_bytes_GT(p) is = %d\n", pairing_length_in_bytes_GT(p));
+    printf("Your pairing_length_in_bytes_Zr(p) is = %d\n", pairing_length_in_bytes_Zr(p));
+
 
     // PBC_ASSERT(!(element_length_in_bytes(masterPublicKey)==pairing_length_in_bytes_G1(p)), "Your master pk element_length_in_bytes is correct");
     // Enable when in debug mode only: add #define PBC_DEBUG
@@ -68,7 +111,7 @@ void init_test(){
 }
 
 
-void FreeElements(){
+void FreeElements(pairing_t p){
     element_free(g);
     element_free(P0);
     element_free(masterPublicKey);
--- api/PBC/setup.h
+++ api/PBC/setup.h
@@ -14,7 +14,9 @@
 #define RBITS 160
 #define QBITS 512
 #define BITS 160
-#define ELEMENT_STRING_LENGTH 1024 // In bytes, both G1, Zr, GT
+#define ELEMENT_STRING_LENGTH 512 // In bytes, both G1, Zr, GT
+#define PARAM_BUFFER_LENGTH_TYPE_A 362 // empirically tested minimal buffer space to store a param string
+#define SIGNATURE_LENGTH 148 // 128+20
 #define DEBUG_MODE 0 // set to 1 to trigger debug mode and see my commented internal variable prints, TODO
 
 /*
@@ -24,11 +26,20 @@
 element_t g; // generator, also is P in Al-Riyami-Patterson03 paper
 element_t masterPublicKey, masterPrivateKey; // master priv/pub key
 element_t P0; // P0
-pbc_param_t par; // pairing params
-pairing_t p; // pairing object
 
-void setup();
-void init_test();
-void FreeElements();
+/*
+ * Use only basic data structures, since it needs I/O
+ */
+struct params {
+    char par_param_buffer[PARAM_BUFFER_LENGTH_TYPE_A];
+    char g[QBITS];
+    char P0[QBITS]; // masterPubKey
+};
+
+void setup(pairing_t p, pbc_param_t par);
+void setup_with_param_buffer(pairing_t p, char * par_param_buffer, int len);
+void init_test(pairing_t p);
+void FreeElements(pairing_t p);
+char * extract_pairing_param_buffer();
 
 #endif //PBC_SETUP_H
