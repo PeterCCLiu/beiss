196c8280e846b159f6529cf709a414c014931a58
zhuaiballl <ghc@mail.bnu.edu.cn>
2022-09-19 14:44:38 +0800

    App.js: add 'readBuff', 'buffToObj', 'writeBuff'

--- api/App.js
+++ api/App.js
@@ -4,17 +4,15 @@ const bodyParser = require('body-parser')
 const {dbStart} = require('./dbOperations');
 const userRouter = require('./route/user');
 const deviceRouter = require('./route/device');
-const {loadWebAssembly} = require('./testWasm');
+const Module = require('./PBC/setup.js');
 
 const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(multipart());
 
-let adder;
-let result;
-const address = './PBC/adder.wasm'
-
+const elementStringLength = 512;
+const paramBufferLengthTypeA = 370;
 
 app.get('/', (req, res) => {
     res.sendStatus(200)
@@ -31,13 +29,40 @@ app.get('/dbSetup', (req, res) => {
 })
 
 app.get('/testWasm', (req, res) => {
-    loadWebAssembly(address)
-        .then(module => {
-            adder = module.instance.exports.adder;
-            result = adder(2, 3);
-            console.log('finished successfully' + result);
-            res.status(200).send('WASM succeeded with result: ' + result);
-        })
+    Module._setup();
+    console.log('finished successfully with result '+ jsStr);
+    res.status(200).send('WASM succeeded with result: ' + jsStr);
+})
+
+app.get('/readBuff', (req, res) => {
+    var pcStr = Module._reader();
+    var jsStr = Module.AsciiToString(pcStr);
+    console.log('finished successfully with result '+ jsStr);
+    res.status(200).send('WASM succeeded with result: ' + jsStr);
+})
+
+app.get('/buffToObj', (req, res) => {
+    var pcStr = Module._reader();
+    var jsStr = Module.AsciiToString(pcStr);
+    console.log('finished reading buffer with result '+ jsStr);
+
+    var obj = new Object();
+    obj.par_param_buffer = jsStr.substr(0, paramBufferLengthTypeA);
+    obj.g                = jsStr.substr(paramBufferLengthTypeA, elementStringLength);
+    obj.P0               = jsStr.substr(paramBufferLengthTypeA+elementStringLength, elementStringLength);
+
+    res.status(200).send(JSON.stringify(obj));
+})
+
+app.get('/writeBuff', (req, res) => {
+    par_param_buffer = req.query.par_param_buffer.padEnd(paramBufferLengthTypeA);
+    g                = req.query.g.padEnd(elementStringLength);
+    P0               = req.query.P0.padEnd(elementStringLength);
+    mystring = par_param_buffer.concat(g).concat(P0);
+    var pcStr = Module._reader();
+    Module.writeStringToMemory(mystring, pcStr);
+    console.log('wrote '+ mystring + ' to buffer');
+    res.status(200).send('done.');
 })
 
 app.use('/users', userRouter);
